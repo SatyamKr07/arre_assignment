0c90a33f7b3048200efdb313b1799d7ec90c8ed1
Satyam Kumar <satyam@LAP-298-XHQL.local>
2024-08-13 00:00:06 +0530

    MediaPlayerContainer ui fixes

--- lib/presentation/media_list/widgets/media_player_container.dart
+++ lib/presentation/media_list/widgets/media_player_container.dart
@@ -17,19 +17,78 @@ class MediaPlayerContainer extends StatelessWidget {
       child: Container(
         height: MediaQuery.of(context).size.height * 0.2,
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: Colors.black,
           borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
         ),
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: [
-            Container(
-              width: 40,
-              height: 5,
-              margin: EdgeInsets.symmetric(vertical: 10),
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(2.5),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Flexible(
+                    flex: 65,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // SizedBox(height: 16),
+                        Text(
+                          "How to make your business grow exponentially",
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          "@ShellySah",
+                          style: TextStyle(color: Colors.tealAccent),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Flexible(
+                    flex: 35,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(
+                          Icons.favorite_border,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(6),
+                          decoration: BoxDecoration(
+                            color: Colors.orange,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Icon(
+                            Icons.play_arrow,
+                            color: Colors.white,
+                            size: 20,
+                          ),
+                        ),
+                        Icon(
+                          Icons.playlist_play,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 5,
+              child: LinearProgressIndicator(
+                backgroundColor: Colors.grey[900],
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.teal),
+                value: 0.4, // Example value, replace with actual percentage
               ),
             ),
           ],
